947d289bdfd9a14bf1c8b9575026e0a8cc7d48bb
lwcc20090709 <115065039+lwcc20090709@users.noreply.github.com>
2022-10-26 14:43:42 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,40 @@
-basic.forever(function () {
-	
+let value_X = 0
+let value_Y = 0
+input.onButtonPressed(Button.A, function () {
+    value_X = 0
+    value_Y = 0
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(value_X, value_Y)
+            basic.pause(500)
+            value_X += 1
+        }
+        value_Y += 1
+        value_X = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    value_X = 0
+    value_Y = 0
+    for (let index = 0; index < 5; index++) {
+        led.plot(value_X, value_Y)
+        basic.pause(500)
+        value_X += 1
+    }
+    for (let index = 0; index < 2; index++) {
+        value_X = 4
+        value_Y += 1
+        for (let index = 0; index < 5; index++) {
+            led.plot(value_X, value_Y)
+            basic.pause(500)
+            value_X += -1
+        }
+        value_X = 0
+        value_Y += 1
+        for (let index = 0; index < 5; index++) {
+            led.plot(value_X, value_Y)
+            basic.pause(500)
+            value_X += 1
+        }
+    }
 })
